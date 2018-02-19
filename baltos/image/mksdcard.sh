58f5e00e098f275bda63de7ca0e3236f62efdaca
Vision Systems GmbH <contact@visionsystems.de>
2018-02-19 17:18:57 +0100

    update to upstream OpenWrt (still experimental)

--- baltos/image/mksdcard.sh
+++ baltos/image/mksdcard.sh
@@ -2,7 +2,7 @@
 
 BOARD_DIR="$(pwd)"
 BOARD_NAME="$(basename ${BOARD_DIR})"
-GENIMAGE_CFG="${BOARD_DIR}/genimage-barebox-${BOARD_NAME}.cfg"
+GENIMAGE_CFG="${BOARD_DIR}/genimage-barebox-baltos.cfg"
 GENIMAGE_TMP="${BOARD_DIR}/../genimage.tmp"
 
 rm -rf "${GENIMAGE_TMP}"
